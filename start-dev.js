454f414431a97df33b24a92c0ea212471e4d5265
amjooky <semy25215@gmail.com>
2025-12-23 14:40:32 +0100

    sync: direct sync from monorepo master logic

--- start-dev.js
+++
@@ -1,7 +0,0 @@
-// Enable debug logging
-process.env.DEBUG = 'express:*';
-
-// Start the server
-require('./server');
-
-console.log('Server started with debug logging enabled');
--- start-with-logging.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Starting server with detailed logging...
-set DEBUG=*
-set NODE_ENV=development
-node simple-server.js
